925bf44d800d5bf344525e32f5aaeae02c7f9a1a
lukehalleran <lukehalleran@users.noreply.github.com>
2025-09-11 14:10:41 -0400

    Update personality_manager.py

--- personality/personality_manager.py
+++ personality/personality_manager.py
@@ -1,20 +1,16 @@
+#/personality/personality_manager.py
 import logging
 
 # Use the root logger or create a child logger that will inherit handlers
 logger = logging.getLogger(__name__)
 logger.debug("Personality manager.py is alive")
 
-import re
-import json
-import os
-from datetime import datetime, timedelta
-import os
 
 class PersonalityManager:
     def __init__(self):
         self.personalities = {
             "default": {
-                "system_prompt_file": "system_prompt_default.txt",
+                "system_prompt_file": "system_prompt.txt",
                 "directives_file": "structured_directives.txt",
                 "num_memories": 5,
                 "include_wiki": True,
@@ -24,7 +20,7 @@ class PersonalityManager:
 
             },
             "therapy": {
-                "system_prompt_file": "system_prompt_therapy.txt",
+                "system_prompt_file": "system_prompt.txt",
                 "directives_file": "structured_directives_therapy.txt",
                 "num_memories": 30,
                 "include_wiki": False,
@@ -34,7 +30,7 @@ class PersonalityManager:
 
             },
             "snarky": {
-                "system_prompt_file": "system_prompt_snarky.txt",
+                "system_prompt_file": "system_prompt.txt",
                 "directives_file": "structured_directives_snarky.txt",
                 "num_memories": 3,
                 "include_wiki": True,
